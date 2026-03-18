b034d30208e101383cbcca4049bc0cdc8b1663ff
MatiasManevi <manevimatias@gmail.com>
2026-03-17 22:01:30 -0300

    log

--- .github/workflows/build_steps.sh
+++ .github/workflows/build_steps.sh
@@ -242,8 +242,8 @@ build_test_wasm() {
     mkdir -p $DIST_DIR/mt
 
     pwd
-    ls build_wasm_st/
-    ls build_wasm_mt/
+    ls build_wasm_st/src
+    ls build_wasm_mt/src
     find build_wasm_st -name "mujoco.js"
     cp build_wasm_st/bin/mujoco.js $DIST_DIR/
     cp build_wasm_st/bin/mujoco.wasm $DIST_DIR/
